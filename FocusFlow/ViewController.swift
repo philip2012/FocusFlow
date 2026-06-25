e303bf17c2a0b51a120b36d889f074d400b6b0c0
Philips Nguyen <99601790+philip2012@users.noreply.github.com>
2026-06-25 20:18:55 +0700

    Connect storyboard outlets and actions

--- FocusFlow/ViewController.swift
+++ FocusFlow/ViewController.swift
@@ -8,7 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var durationSlider: UISlider!
+    @IBOutlet weak var soundSelector: UISegmentedControl!
+    @IBOutlet weak var volumeLabel: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    // IBActions
+    @IBAction func startTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func pauseTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func resetTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func durationChanged(_ sender: UISlider) {
+        
+    }
+    @IBAction func soundChanged(_ sender: UISegmentedControl) {
+        
+    }
+    @IBAction func volumeChanged(_ sender: UISlider) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
