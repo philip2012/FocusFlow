37c1c2f31d856072f4f67cfc31aac499b43f9e3a
Philips Nguyen <nvananphuc2022@gmail.com>
2026-06-25 19:25:02 +0700

    Initial Commit

---
+++ FocusFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusFlow
+//
+//  Created by Nguyễn Vạn An Phúc on 25/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
